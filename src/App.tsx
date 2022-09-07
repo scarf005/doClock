2df0c013d1c80407d4cead1b707c96b72604ed0c
scarf <greenscarf005@gmail.com>
2022-09-07 10:13:46 +0900

    feat: add clock markers

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import { Center, SimpleGrid, Stack, Title } from '@mantine/core'
+import { Center, SimpleGrid, Stack } from '@mantine/core'
 import { useListState } from '@mantine/hooks'
-import { OFFSET, Rotate, TodoInput, TodoItem, TodoList } from 'components'
+import { OFFSET, Clock, Rotate, TodoInput, TodoItem, TodoList } from 'components'
 import { Todo } from 'data'
-import dayjs from 'dayjs'
-import { useClock } from 'hooks'
 import { dateToDegree, degreesToRadian } from 'utility'
 import './index.css'
-
-const Clock = ({ is24Clock = false }: { is24Clock?: boolean }) => (
-  <Title order={1} size="8vh">
-    {dayjs(useClock()).format(is24Clock ? 'HH:mm:ss' : 'hh:mm:ssa') + ' >'}
-  </Title>
-)
 
 export const App = () => {
   const [todos, { append, remove }] = useListState<Todo>([
     new Todo('새벽', new Date('2022-09-06 00:00:00')),
     new Todo('저녁 약속', new Date('2022-09-06 6:00:00')),
-    new Todo('장보러 가기', new Date('2022-09-06 12:00:00')),
-    new Todo('do something', new Date('2022-09-06 18:00:00')),
+    // new Todo('장보러 가기', new Date('2022-09-06 12:00:00')),
+    // new Todo('do something', new Date('2022-09-06 18:00:00')),
   ])
 
   return (
@@ -26,12 +18,7 @@ export const App = () => {
       <Center style={{ height: '80vh' }}>
         <Clock />
         {todos.map((todo, i) => (
-          <Rotate
-            radian={degreesToRadian(
-              dateToDegree(todo.date, true) - dateToDegree(new Date(), true)
-            )}
-            offset={OFFSET}
-          >
+          <Rotate radian={degreesToRadian(dateToDegree(todo.date))}>
             <TodoItem remove={() => remove(i)}>{todo.title}</TodoItem>
           </Rotate>
         ))}
--- src/utility/dateUtils.test.ts
+++ src/utility/dateUtils.test.ts
@@ -1,8 +1,17 @@
 import { expect, test } from 'vitest'
-import { dateToTime24h } from './dateUtils'
+import { dateToDegree, dateToTime24h } from './dateUtils'
 
 test('dateToTime24h', () => {
   const d = new Date('2021-01-01T12:34:56.789')
   expect(dateToTime24h(d)).toEqual([12, 34, 56])
 })
 
+test('dateToDegree', () => {
+  // 12hour
+  expect(dateToDegree(new Date('2021-01-01T00:00:00.000'))).toBe(
+    dateToDegree(new Date('2021-01-01T12:00:00.000'))
+  )
+  expect(dateToDegree(new Date('2021-01-01T00:00:00.000'))).toBe(
+    dateToDegree(new Date('2021-01-02T00:00:00.000'))
+  )
+})
