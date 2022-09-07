cf4ecc50e1d5e747dad110af539b78bd0491b2cb
scarf <greenscarf005@gmail.com>
2022-09-07 10:13:01 +0900

    refactor: use option for Rotate

--- src/components/Rotate.tsx
+++ src/components/Rotate.tsx
@@ -3,18 +3,26 @@ import { dateToDegree, degreesToRadian } from 'utility'
 export const QUARTER_CIRCLE = 1.57079633
 export const OFFSET = '34vh'
 
+interface RotateOption {
+  offset?: number | string
+  relative?: boolean
+}
+const defaultOption = { offset: OFFSET, relative: true }
 interface RotateProps {
   radian: number
-  offset?: number | string
+  option?: RotateOption
   children: React.ReactNode
 }
-export const Rotate = ({ radian, offset = 0, children }: RotateProps) => {
+export const Rotate = ({ radian, option = {}, children }: RotateProps) => {
+  const opt = { ...defaultOption, ...option }
+  const rad = opt.relative ? radian - dateToDegree(new Date()) : radian
+
   return (
     <div
       key={radian}
       style={{
         position: 'absolute',
-        transform: `rotateZ(${radian}rad) translateX(${offset})`,
+        transform: `rotateZ(${rad}rad) translateX(${opt.offset})`,
         transformOrigin: 'center',
       }}
     >
