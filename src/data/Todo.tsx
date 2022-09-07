02b644c5af8903c160eb3a60c8fc252f58870ce5
scarf <greenscarf005@gmail.com>
2022-09-07 14:46:07 +0900

    feat: add JSON conversion for Todo

--- src/data/Todo.tsx
+++ src/data/Todo.tsx
@@ -1,5 +1,7 @@
 import dayjs from 'dayjs'
 import { nanoid } from 'nanoid'
+
+type rawTodo = { title: string; date: string; id: string }
 export class Todo {
   constructor(
     public title: string,
@@ -9,5 +11,16 @@ export class Todo {
 
   toString() {
     return `${dayjs(this.date).format('HH:mm:ss')}: ${this.title}`
+  }
+
+  static fromJSON(json: string) {
+    const { title, date, id } = JSON.parse(json)
+    return new Todo(title, new Date(date), id)
+  }
+
+  static fromJSONList(json: string) {
+    return (JSON.parse(json) as rawTodo[]).map(
+      ({ title, date, id }: rawTodo) => new Todo(title, new Date(date), id)
+    )
   }
 }
