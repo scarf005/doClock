180a3ee4a81c1cdcc3defc664cdd611430462ee1
scarf <greenscarf005@gmail.com>
2022-09-07 14:46:53 +0900

    refactor: change to useLocalStorageList

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
 import { Center, SimpleGrid, Stack } from '@mantine/core'
-import { useListState } from '@mantine/hooks'
-import {
-  OFFSET,
-  Clock,
-  Rotate,
-  TodoInput,
-  TodoItem,
-  TodoList,
-} from 'components'
+import { Clock, Rotate, TodoInput, TodoItem, TodoList } from 'components'
 import { Todo } from 'data'
+import { useLocalStorageList } from 'hooks'
 import { dateToDegree, degreesToRadian } from 'utility'
 import './index.css'
 
 export const App = () => {
-  const [todos, { append, remove }] = useListState<Todo>([
-    new Todo('새벽', new Date('2022-09-06 00:00:00')),
-    new Todo('3시', new Date('2022-09-06 03:00:00')),
-    new Todo('저녁 약속', new Date('2022-09-06 6:00:00')),
-    new Todo('10시', new Date('2022-09-06 10:00:00')),
-    new Todo('10시 30분', new Date('2022-09-06 10:30:00')),
-    // new Todo('장보러 가기', new Date('2022-09-06 12:00:00')),
-    // new Todo('do something', new Date('2022-09-06 18:00:00')),
-  ])
+  const [todos, { append, remove }] = useLocalStorageList<Todo>({
+    key: 'todos',
+    defaultValue: [
+      new Todo('새벽', new Date('2022-09-06 00:00:00')),
+      new Todo('3시', new Date('2022-09-06 03:00:00')),
+      new Todo('저녁 약속', new Date('2022-09-06 6:00:00')),
+      new Todo('10시', new Date('2022-09-06 10:00:00')),
+      new Todo('10시 30분', new Date('2022-09-06 10:30:00')),
+      // new Todo('장보러 가기', new Date('2022-09-06 12:00:00')),
+      // new Todo('do something', new Date('2022-09-06 18:00:00')),
+    ],
+    deserialize: value => Todo.fromJSONList(value),
+  })
+  console.log('todos', todos)
 
   return (
     <SimpleGrid cols={2} spacing="xl">
@@ -29,7 +27,7 @@ export const App = () => {
         <Clock />
         {todos.map((todo, i) => (
           <Rotate
-            key={`${todo.date}`}
+            key={todo.id}
             radian={degreesToRadian(dateToDegree(todo.date))}
           >
             <TodoItem remove={() => remove(i)}>{todo.title}</TodoItem>
