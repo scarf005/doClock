efda7c140a6f0ba40134e4a5533edb2a9b1f604b
scarf <greenscarf005@gmail.com>
2022-09-08 01:33:18 +0900

    feat: atomWithToggleStorage

--- src/atom/atomWithToggle.ts
+++ src/atom/atomWithToggle.ts
@@ -1,9 +1,27 @@
 import { atom } from 'jotai'
+import { atomWithStorage } from 'jotai/utils'
 
-export const atomWithToggle = <T>(value: [T, T] = [false, true] as [T, T]) => {
-  const anAtom = atom(value[0], (get, set) => {
-    set(anAtom, value[1 - value.indexOf(get(anAtom))])
+const otherValue = <T>(values: [T, T], value: T) =>
+  values[1 - values.indexOf(value)]
+
+export const atomWithToggle = <T>(values: [T, T] = [false, true] as [T, T]) => {
+  const anAtom = atom(values[0], (get, set) => {
+    set(anAtom, otherValue(values, get(anAtom)))
   })
 
   return anAtom
+}
+
+export const atomWithToggleStorage = <T>(
+  key: string,
+  values: [T, T] = [false, true] as [T, T],
+  storage?: any
+) => {
+  const baseAtom = atomWithStorage(key, values[0], storage)
+  const derivedAtom = atom(
+    get => get(baseAtom),
+    (get, set) => set(baseAtom, otherValue(values, get(baseAtom)))
+  )
+
+  return derivedAtom
 }
--- src/atom/timeMode.ts
+++ src/atom/timeMode.ts
@@ -1,4 +1,7 @@
-import { atomWithToggle } from './atomWithToggle'
+import { atomWithToggle, atomWithToggleStorage } from './atomWithToggle'
 
 export type TimeMode = '24h' | '12h'
-export const timeModeAtom = atomWithToggle<TimeMode>(['12h', '24h'])
+export const timeModeAtom = atomWithToggleStorage<TimeMode>('timemode', [
+  '12h',
+  '24h',
+])
