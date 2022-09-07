458ad3c4756b3278e8fa6b730616230faf2362b3
scarf <greenscarf005@gmail.com>
2022-09-07 13:24:43 +0900

    feat: id for Todo

--- src/data/Todo.tsx
+++ src/data/Todo.tsx
@@ -1,7 +1,11 @@
 import dayjs from 'dayjs'
-
+import { nanoid } from 'nanoid'
 export class Todo {
-  constructor(public title: string, public date: Date = new Date()) {}
+  constructor(
+    public title: string,
+    public date: Date = new Date(),
+    readonly id = nanoid()
+  ) {}
 
   toString() {
     return `${dayjs(this.date).format('HH:mm:ss')}: ${this.title}`
