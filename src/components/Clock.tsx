c09b23d95ccccd7aea2592f3c65dc2ff1bd33aa7
scarf <greenscarf005@gmail.com>
2022-09-08 18:09:48 +0900

    feat: clock hands

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,6 +1,6 @@
 import { Title } from '@mantine/core'
-import { IconMinus } from '@tabler/icons'
-import { timeModeAtom } from 'atom'
+import { IconArrowNarrowRight, IconArrowRight, IconMinus } from '@tabler/icons'
+import { isAbsoluteRotation, timeModeAtom } from 'atom'
 import { Rotate } from 'components'
 import dayjs from 'dayjs'
 import { useClock } from 'hooks'
@@ -8,9 +8,12 @@ import { useAtomValue } from 'jotai'
 import { ReactElement } from 'react'
 import {
   degreesToRadian,
+  hourHandToRadian,
   HOUR_DEGREE,
-  MINUTE_LINE_DEGREE,
+  minuteHandToRadian,
+  MINUTE_HAND_DEGREE,
   partition,
+  secondHandToRadian,
 } from 'utility'
 
 interface MarkerProps {
@@ -26,29 +29,61 @@ const Markers = ({ ns, icon }: MarkerProps) => (
     ))}
   </>
 )
-const [hours12, minutes12] = partition(
-  Array.from({ length: 60 }, (_, i) => i * MINUTE_LINE_DEGREE),
-  x => x % HOUR_DEGREE === 0
-)
-const [hours24, minutes24] = partition(
-  Array.from({ length: 120 }, (_, i) => (i * MINUTE_LINE_DEGREE) / 2),
-  x => x % (HOUR_DEGREE / 2) === 0
-)
+interface HandProps {
+  radian: number
+  icon: ReactElement
+}
+const Hand = ({ radian, icon }: HandProps) => {
+  return (
+    <Rotate radian={radian} offset="26vh">
+      {icon}
+    </Rotate>
+  )
+}
+
+const getMarkers = (div: 1 | 2 = 1) =>
+  partition(
+    Array.from({ length: 60 * div }, (_, i) => (i * MINUTE_HAND_DEGREE) / div),
+    x => x % (HOUR_DEGREE / div) === 0
+  )
+
 export const Clock = () => {
+  const isAbsolute = useAtomValue(isAbsoluteRotation)
   const mode = useAtomValue(timeModeAtom)
+  const [hour, minute] = getMarkers((['12h', '24h'].indexOf(mode) + 1) as 1 | 2)
   const time = dayjs(useClock()).format(
     mode === '12h' ? 'hh:mm:ssa' : 'HH:mm:ss'
   )
-  const [hours, minutes] =
-    mode === '12h' ? [hours12, minutes12] : [hours24, minutes24]
 
   return (
     <>
       <Title order={1} size="8vh">
-        {time + ' >'}
+        {time}
       </Title>
-      <Markers ns={hours} icon={<IconMinus size={8} />} />
-      <Markers ns={minutes} icon={<IconMinus size={5} />} />
+      <Markers ns={hour} icon={<IconMinus size={8} />} />
+      <Markers ns={minute} icon={<IconMinus size={5} />} />
+      {isAbsolute ? (
+        <>
+          <Hand
+            radian={secondHandToRadian()}
+            icon={<IconMinus size={20} color="#eb4034" />}
+          />
+          <Hand
+            radian={minuteHandToRadian()}
+            icon={<IconMinus size={20} color="#f0a36c" />}
+          />
+          <Hand
+            radian={hourHandToRadian()}
+            icon={<IconArrowNarrowRight size={20} color="#f2de94" />}
+          />
+        </>
+      ) : (
+        <IconMinus
+          size={20}
+          color="#eb4034"
+          style={{ transform: 'translateX(5vh)' }}
+        />
+      )}
     </>
   )
 }
